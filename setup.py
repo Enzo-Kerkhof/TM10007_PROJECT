34a1ff00d97a0e23aecc237f698d48f0d737063f
Enzo-Kerkhof <Enzo.Kerkhof@gmail.com>
2020-03-28 12:21:40 +0100

    Add to readme, test setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name="brats",
+    name="adni",
     version="0.2",
-    description="""Final Project TM10007ML""",
+    description="""Final Project TM10007ML group adni git""",
     license="Apache 2.0 License",
     author="Martijn Starmans, Karin van Garderen, Hakim Achterberg",
     author_email="m.starmans@erasmusmc.nl",
     include_package_data=True,
     packages=[
-        "brats",
-        "hn",
         "adni"
     ],
 )
